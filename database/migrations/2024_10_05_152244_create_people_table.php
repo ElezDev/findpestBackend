2a88ebeeae91793d3b39209c48c4264f8fab94a6
ElezDev <edwinledezma2013@gmail.com>
2025-01-18 15:41:37 -0500

    Eliminar duplicado de la columna user_id en la migración de la tabla people

--- database/migrations/2024_10_05_152244_create_people_table.php
+++ database/migrations/2024_10_05_152244_create_people_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Relación con la tabla `users`
             $table->text('biography')->nullable();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('image_url');
             $table->timestamps();
         });
